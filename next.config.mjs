80dbda080131f44cfbdf4f589c003dbfabfd186a
lukxce <77982746+lukxce@users.noreply.github.com>
2026-05-13 12:35:36 +0200

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "fastly.picsum.photos",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
